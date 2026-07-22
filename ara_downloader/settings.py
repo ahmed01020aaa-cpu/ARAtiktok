e43582d54b1fdda098a68f8ea9483c0451ce4b30
ahmed01020aaa-cpu <ahmed01020aaa@gmail.com>
2026-07-22 12:38:49 +0300

    update

--- ara_downloader/settings.py
+++ ara_downloader/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0zlziyb*i$3__71gmz2555y5=9y7ad#tc&eth9ecvig_=tm)g'
+import os
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
