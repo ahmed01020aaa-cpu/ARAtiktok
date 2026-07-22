c1e753cb99b9b008f9ca50514b1fb87d3c7beb14
ahmed01020aaa-cpu <ahmed01020aaa@gmail.com>
2026-07-22 13:16:28 +0300

    update

--- ara_downloader/urls.py
+++ ara_downloader/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 from django.views.generic import RedirectView
-from downloader import views
-from  downloader.views import google_verification
 from django.contrib.sitemaps.views import sitemap
+
+from downloader import views
+from downloader.views import google_verification
 from .sitemaps import StaticViewSitemap
-from downloader.views import robots_txt
 
 sitemaps = {
     "static": StaticViewSitemap,
 }
 
-
-
-
 urlpatterns = [
- 
+    path("", views.index, name="index"),
+    path("api/fetch", views.fetch_info, name="fetch_info"),
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon.svg", permanent=False)),
+
     path(
         "sitemap.xml",
         sitemap,
@@ -23,20 +23,6 @@ urlpatterns = [
     ),
 
     path("robots.txt", views.robots_txt),
-
-    path(
-        "google55e2cfdb79c0b019.html",
-        google_verification,
-        name="google_verification",
-    ),
-]
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("api/fetch", views.fetch_info, name="fetch_info"),
-    path("favicon.ico", RedirectView.as_view(url="/static/favicon.svg", permanent=False)),
-
-
 
     path(
         "google55e2cfdb79c0b019.html",
