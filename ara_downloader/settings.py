d20c34c040ceb6083164910752f3c552ee967fe0
ahmed01020aaa-cpu <ahmed01020aaa@gmail.com>
2026-07-27 18:11:31 +0300

    update

--- ara_downloader/settings.py
+++ ara_downloader/settings.py
@@ -24,7 +24,7 @@ import os
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
